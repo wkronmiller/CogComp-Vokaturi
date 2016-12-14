7fceb93194a33d68d839a4d1c280046184e0243b
Adam Lawrence <adam.ad.lawrence@gmail.com>
2016-12-13 23:11:40 -0500

    audio_data to wave conversation, then to google API

--- WatsonAnalyzer/lib/main.py
+++ WatsonAnalyzer/lib/main.py
@@ -10,6 +10,11 @@ import json
 from os.path import join, dirname
 from watson_developer_cloud import SpeechToTextV1
 import speech_recognition as sr
+from os import path
+import wave
+import pyaudio
+audio_group = []
+
 
 def _handle_audio(audio_data_):# pylint: disable=unused-argument
     """
@@ -19,23 +24,33 @@ def _handle_audio(audio_data_):# pylint: disable=unused-argument
     #print "TODO: process audio data thru watson here"
 
     #!/usr/bin/env python3
+    p = pyaudio.PyAudio()
+
+    wf = wave.open("temp_audio.wav", 'wb')
+    wf.setnchannels(1)
+    wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
+    wf.setframerate(44100)
+    wf.writeframes(audio_data_)
+    wf.close()
 
 
-
-
-    audio_data = sr.AudioData(audio_data_, RATE, 2)
+    #audio_group.append(audio_data_)
+    #audio_data = sr.AudioData(audio_data_, RATE, 2)
+    AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "temp_audio.wav")
 
     # use the audio file as the audio source
     r = sr.Recognizer()
-    #with sr.AudioFile(AUDIO_FILE) as source:
-    #    audio = r.record(source) # read the entire audio file
+    with sr.AudioFile(AUDIO_FILE) as source:
+        audio = r.record(source) # read the entire audio file
+
+
 
     # recognize speech using Google Speech Recognition
     try:
         # for testing purposes, we're just using the default API key
         # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
         # instead of `r.recognize_google(audio)`
-        print("Google Speech Recognition thinks you said " + r.recognize_google(audio_data))
+        print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
     except sr.RequestError as e:
