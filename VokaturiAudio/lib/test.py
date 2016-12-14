d1dadba892b92a57eac76a0c4aad1d463884addf
William Rory Kronmiller <wrk961@gmail.com>
2016-12-13 20:49:30 -0500

    Added in-sample error testing to test script

--- VokaturiAudio/lib/test.py
+++ VokaturiAudio/lib/test.py
@@ -2,5 +2,25 @@
 """
 Test ML model
 """
-#from main import test_model
-#TODO
+import loader
+from learning import test_model
+
+def _get_in_sample(trained_model):
+    """
+    Get the in-sample error
+    """
+    train_features = loader.get_feature_map()
+    in_sample_error = test_model(trained_model, train_features)
+
+    print "In sample error", in_sample_error
+
+def main():
+    """
+    Perform accuracy tests on model
+    """
+    trained_model = loader.load_model()
+
+    _get_in_sample(trained_model)
+
+if __name__ == "__main__":
+    main()
